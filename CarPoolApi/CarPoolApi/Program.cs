fdac6d4fc6936b38465536884b3ee2b3baf68b89
chandan5614 <chandan.dy1995@gmail.com>
2024-08-20 20:41:46 -0500

    Moved keys and secrets to env file

--- CarPoolApi/CarPoolApi/Program.cs
+++ CarPoolApi/CarPoolApi/Program.cs
@@ -8,8 +8,12 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Azure.Cosmos;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load .env file
+Env.Load();
 
 // Configure the URLs to be used by the application
 builder.WebHost.UseUrls("https://localhost:7026", "http://localhost:5026");
@@ -19,7 +23,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 // JWT Authentication configuration
-var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Secret"]);
+var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET"));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -69,7 +73,7 @@ builder.Services.AddSwaggerGen(c =>
 // Add Cosmos DB configuration
 builder.Services.AddSingleton(provider =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("CosmosDb");
+    var connectionString = Environment.GetEnvironmentVariable("COSMOSDB_CONNECTION_STRING");
     if (string.IsNullOrWhiteSpace(connectionString))
     {
         throw new InvalidOperationException("Cosmos DB connection string is not configured properly.");
@@ -81,48 +85,48 @@ builder.Services.AddSingleton(provider =>
 builder.Services.AddScoped<IUserRepository>(provider =>
 {
     var cosmosClient = provider.GetRequiredService<CosmosClient>();
-    var databaseName = builder.Configuration["ConnectionStrings:DatabaseName"];
-    var containerName = builder.Configuration["ConnectionStrings:UserContainerName"];
+    var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+    var containerName = Environment.GetEnvironmentVariable("USER_CONTAINER_NAME");
     return new CosmosDbUserRepository(cosmosClient, databaseName, containerName);
 });
 
 builder.Services.AddScoped<IBookingRepository>(provider =>
 {
     var cosmosClient = provider.GetRequiredService<CosmosClient>();
-    var databaseName = builder.Configuration["ConnectionStrings:DatabaseName"];
-    var containerName = builder.Configuration["ConnectionStrings:BookingContainerName"];
+    var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+    var containerName = Environment.GetEnvironmentVariable("BOOKING_CONTAINER_NAME");
     return new CosmosDbBookingRepository(cosmosClient, databaseName, containerName);
 });
 
 builder.Services.AddScoped<IRideRepository>(provider =>
 {
     var cosmosClient = provider.GetRequiredService<CosmosClient>();
-    var databaseName = builder.Configuration["ConnectionStrings:DatabaseName"];
-    var containerName = builder.Configuration["ConnectionStrings:RideContainerName"];
+    var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+    var containerName = Environment.GetEnvironmentVariable("RIDE_CONTAINER_NAME");
     return new CosmosDbRideRepository(cosmosClient, databaseName, containerName);
 });
 
 builder.Services.AddScoped<IPaymentRepository>(provider =>
 {
     var cosmosClient = provider.GetRequiredService<CosmosClient>();
-    var databaseName = builder.Configuration["ConnectionStrings:DatabaseName"];
-    var containerName = builder.Configuration["ConnectionStrings:PaymentContainerName"];
+    var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+    var containerName = Environment.GetEnvironmentVariable("PAYMENT_CONTAINER_NAME");
     return new CosmosDbPaymentRepository(cosmosClient, databaseName, containerName);
 });
 
 builder.Services.AddScoped<IRatingRepository>(provider =>
 {
     var cosmosClient = provider.GetRequiredService<CosmosClient>();
-    var databaseName = builder.Configuration["ConnectionStrings:DatabaseName"];
-    var containerName = builder.Configuration["ConnectionStrings:RatingContainerName"];
+    var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+    var containerName = Environment.GetEnvironmentVariable("RATING_CONTAINER_NAME");
     return new CosmosDbRatingRepository(cosmosClient, databaseName, containerName);
 });
 
 builder.Services.AddScoped<IScheduleRepository>(provider =>
 {
     var cosmosClient = provider.GetRequiredService<CosmosClient>();
-    var databaseName = builder.Configuration["ConnectionStrings:DatabaseName"];
-    var containerName = builder.Configuration["ConnectionStrings:ScheduleContainerName"];
+    var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+    var containerName = Environment.GetEnvironmentVariable("SCHEDULE_CONTAINER_NAME");
     return new CosmosDbScheduleRepository(cosmosClient, databaseName, containerName);
 });
 
